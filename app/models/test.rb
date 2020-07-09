88e3195d0da9dd0021b8981143bb30f024a1758d
Anakin Skywalker <mutovin9@mail.ru>
2020-07-09 12:13:03 -0400

    'Задание к уроку "Интерфейс запросов ActiveRecord".Выполнение всех заданий'

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  def self.sort_descending(category_title)
+    Test.joins(:category).where(categories: { title: category_title }).order('tests.title DESC').pluck('tests.title')
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def show_all_tests(level)
+    Test.where(level: level)
+  end
 end
