95e87fa4c48437376e93c9f2b80d4ddf9efc2325
Anakin Skywalker <mutovin9@mail.ru>
2020-07-14 12:23:49 -0400

    'Задание к уроку "Ассоциации моделей".Выполнение задания'

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
   def self.sort_descending(category_title)
-    joins('JOIN categories ON categories.id == tests.category_id').where(categories: { title: category_title }).order(title: :desc).pluck(:title)
+    joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_133941) do
+ActiveRecord::Schema.define(version: 2020_07_14_160045) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2020_07_12_133941) do
     t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "author_id"
+    t.index ["author_id"], name: "index_tests_on_author_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
@@ -54,7 +56,7 @@ ActiveRecord::Schema.define(version: 2020_07_12_133941) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
