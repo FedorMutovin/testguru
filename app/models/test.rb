63fc3f9527a0e65584359f00dbae6cef4f779b53
Anakin Skywalker <mutovin9@mail.ru>
2020-07-15 01:06:51 -0400

    'Задание к уроку "Ассоциации моделей".Исправление замечаний'

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,9 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :questions
-  has_many :tests_users
+  belongs_to :author, class_name: 'User'
+  has_many :questions, dependent: :destroy
+  has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
-  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   def self.sort_descending(category_title)
     joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
