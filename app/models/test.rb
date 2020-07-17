83435e0329d26fab72a6abf29c109657534c9df8
Anakin Skywalker <mutovin9@mail.ru>
2020-07-16 23:41:27 -0400

    'Задание к уроку "Ассоциации моделей".Финальные исправления'

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category, optional: true
   belongs_to :author, class_name: 'User'
-  has_many :questions, dependent: :nullify
+  has_many :questions, dependent: :destroy
   has_many :tests_users, dependent: :destroy
   has_many :users, through: :tests_users
 
