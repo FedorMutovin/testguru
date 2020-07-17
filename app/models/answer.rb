f8ff9900e83a9daecd0da472ce58c6ab4dbc76cd
Anakin Skywalker <mutovin9@mail.ru>
2020-07-17 11:36:59 -0400

    'Задание к уроку "Валидации и scope-методы".Рефакторинг'

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,10 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  scope :correct_answers, -> { where(correct: true) }
-  validates :body, presence: true
 
+  validates :body, presence: true
   validate :validate_answers_count
+
+  scope :correct_answers, -> { where(correct: true) }
 
   private
 
