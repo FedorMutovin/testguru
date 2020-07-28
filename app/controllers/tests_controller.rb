6ca6606563f1e9a996dcd5c45b9919af29358861
Anakin Skywalker <mutovin9@mail.ru>
2020-07-28 13:26:58 -0400

    'Задание к уроку "Rails Views".исправления хелпера вопросов, доработка тест контроллера, фикс паршалов форм'

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-  before_action :find_test, only: %i[destroy update show]
+  before_action :find_test, only: %i[destroy update show edit]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
   def index
     @tests = Test.all
