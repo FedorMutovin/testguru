03a9e93489474ffce50706ab3ac90fdbc2ff63a7
Anakin Skywalker <mutovin9@mail.ru>
2020-08-16 11:29:12 -0400

    "Аутентификация с Devise.исправление последних замечаний"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(user)
-    flash[:notice] = "Привет, #{user.first_name} #{user.last_name}"
     user.admin? ? admin_tests_path : tests_path
   end
 
