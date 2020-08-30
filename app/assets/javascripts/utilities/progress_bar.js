2bba885f2d4e49f9e4ce42312d2a32480e1ffcec
Anakin Skywalker <mutovin9@mail.ru>
2020-08-30 12:56:13 -0400

    "data-attributes.выполение всех заданий"

--- app/assets/javascripts/utilities/progress_bar.js
+++ app/assets/javascripts/utilities/progress_bar.js
@@ -1,6 +1,5 @@
 document.addEventListener('turbolinks:load', function () {
     const progressBar = document.querySelector('.progress-bar');
-    console.log(typeof progressBar)
     const { progress } = progressBar.dataset;
 
     progressBar.style.width = `${progress}%`;
