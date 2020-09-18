deb39b6ca0a6f8423f7a07a0ef2bae633edd8af2
Anakin Skywalker <mutovin9@mail.ru>
2020-09-18 13:39:09 -0400

    "Бэйджи.изменена логика выдачи

--- app/services/badge_issuance.rb
+++ app/services/badge_issuance.rb
@@ -9,19 +9,26 @@ class BadgeIssuance
   end
 
   def first_try(badge)
-    @user.tests.where(title: @test_passage.test.title).count.eql?(1) && @test_passage.successful
+    @user.tests.where(title: @test_passage.test.title).count.eql?(badge.rule_value.to_i) && @test_passage.successful
   end
 
   def category(badge)
-    return unless @test_passage.test.category.eql?(badge.rule_value)
+    return false unless @test_passage.test.category.title.eql?(badge.rule_value)
 
     Test.where(category: Category.find_by(title: badge.rule_value)).each do |test|
       tests = @user.tests.where(title: test.title)
-      return false unless tests.present? && tests.test_passages.successful_completed.present?
+      return false unless tests.present? && tests.where('test_passages.successful').first.present?
     end
+    true
   end
 
-  def level
-    Badge.level
+  def level(badge)
+    return false unless @test_passage.test.level.eql?(badge.rule_value.to_i)
+
+    Test.where(level: badge.rule_value.to_i).each do |test|
+      tests = @user.tests.where(title: test.title)
+      return false unless tests.present? && tests.where('test_passages.successful').first.present?
+    end
+    true
   end
 end
