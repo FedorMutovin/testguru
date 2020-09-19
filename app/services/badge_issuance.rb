860482cfd5bcc23665fe16f02847913db94c0ab5
Anakin Skywalker <mutovin9@mail.ru>
2020-09-19 14:11:04 -0400

    "Бэйджи.рефакторинг"

--- app/services/badge_issuance.rb
+++ app/services/badge_issuance.rb
@@ -4,6 +4,8 @@ class BadgeIssuance
     @user = test_passage.user
   end
 
+  RULES = %w[category level first_try].freeze
+
   def give_badges
     Badge.select { |badge| send badge.rule_type, badge }
   end
@@ -13,22 +15,23 @@ class BadgeIssuance
   end
 
   def category(badge)
+    return false if @user.badges.include?(badge)
     return false unless @test_passage.test.category.title.eql?(badge.rule_value)
 
-    Test.where(category: Category.find_by(title: badge.rule_value)).each do |test|
-      tests = @user.tests.where(title: test.title)
-      return false unless tests.present? && tests.where('test_passages.successful').first.present?
-    end
-    true
+    category_tests_ids = Test.where(category: Category.find_by(title: badge.rule_value)).select(:id)
+    user_tests_ids = @user.tests.where(category: Category.find_by(title: badge.rule_value))
+                          .where('test_passages.successful').select(:id)
+
+    (category_tests_ids - user_tests_ids).empty?
   end
 
   def level(badge)
+    return false if @user.badges.include?(badge)
     return false unless @test_passage.test.level.eql?(badge.rule_value.to_i)
 
-    Test.where(level: badge.rule_value.to_i).each do |test|
-      tests = @user.tests.where(title: test.title)
-      return false unless tests.present? && tests.where('test_passages.successful').first.present?
-    end
-    true
+    level_tests_ids = Test.where(level: badge.rule_value.to_i).select(:id)
+    user_tests_ids = @user.tests.where(level: badge.rule_value.to_i).where('test_passages.successful').select(:id)
+
+    (level_tests_ids - user_tests_ids).empty?
   end
 end
